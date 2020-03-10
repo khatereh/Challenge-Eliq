be7a9fee552168b17cc5f36b35cb6433b1c62343
Khatereh <Ghasemihedaiat@gmail.com>
2020-03-10 18:24:18 +0330

    Initial Commit

---
+++ Challenge-Eliq/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge-Eliq
+//
+//  Created by parto on 3/10/20.
+//  Copyright © 2020 parto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
